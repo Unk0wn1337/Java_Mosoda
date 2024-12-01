1fd2d5791efb5a99330f7a2e17fe77fea34c68c5
Unk0wn1337 <kelogames@gmail.com>
2024-12-01 22:35:37 +0100

    javitott

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -1,7 +1,32 @@
 package hu.szamalk;
 
+import hu.szamalk.model.Mosoda;
+import hu.szamalk.model.Ruha;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Mosoda mosoda = new Mosoda(5);
+
+
+        Ruha ruha1 = new Ruha("Péter");
+        Ruha ruha2 = new Ruha("Anna");
+        Ruha ruha3 = new Ruha("János");
+
+        mosoda.bevesz(ruha1);
+        mosoda.bevesz(ruha2);
+        mosoda.bevesz(ruha3);
+
+
+        mosoda.mos();
+
+
+        Ruha kiadottRuha = mosoda.kiad("Anna");
+        if (kiadottRuha != null) {
+            System.out.println("Kiadott ruha: " + kiadottRuha);
+        }
+
+        // Próbálkozás megtelt mosodával
+        Ruha ruha4 = new Ruha("László");
+        mosoda.bevesz(ruha4);  // Ez a ruha már nem kerül be, ha megtelt
     }
 }
--- src/main/java/hu/szamalk/model/Mosoda.java
+++ src/main/java/hu/szamalk/model/Mosoda.java
@@ -1,7 +1,8 @@
 package hu.szamalk.model;
 
-public class Mosoda  {
-   private Ruha[] ruhak;
+public class Mosoda {
+    private Ruha[] ruhak;
+
 
     public Mosoda() {
         this.ruhak = new Ruha[5];
@@ -11,34 +12,45 @@ public class Mosoda  {
         this.ruhak = new Ruha[db];
     }
 
-    public void bevesz(Ruha ruha){
-        int i = 0;
-        while(i < ruhak.length){
-            ruhak[i] = ruha;
-            i++;
+
+    public void bevesz(Ruha ruha) {
+        for (int i = 0; i < ruhak.length; i++) {
+            if (ruhak[i] == null) {
+                ruhak[i] = ruha;
+                System.out.println("Ruha sikeresen felvéve: " + ruha.getTulaj());
+                return;
+            }
         }
+        System.out.println("A mosoda megtelt!");
     }
 
-
-    public Ruha kiad(Ruha r){
-        int i = 0;
-        while(i < ruhak.length & ruhak[i] == r){
-            i++;
+    public Ruha kiad(String tulaj) {
+        for (int i = 0; i < ruhak.length; i++) {
+            if (ruhak[i] != null && ruhak[i].getTulaj().equals(tulaj)) {
+                Ruha kiadottRuha = ruhak[i];
+                ruhak[i] = null;
+                System.out.println("Ruha kiadva a tulajdonosnak: " + tulaj);
+                return kiadottRuha;
+            }
         }
-        ruhak.splice(i);
-
-        return r;
+        System.out.println("Nincs ilyen tulajdonú ruha a mosodában!");
+        return null;
     }
 
-    public void mos(){
-        for(int i = 0; i < ruhak.length; i++){
-            if(ruhak[i].isTiszta() == false){
+        public void mos() {
+        for (int i = 0; i < ruhak.length; i++) {
+            if (ruhak[i] != null && !ruhak[i].isTiszta()) {
                 ruhak[i].setTiszta(true);
+                System.out.println("Ruha tisztítva: " + ruhak[i].getTulaj());
             }
         }
     }
 
+    public void listaz() {
+        for (int i = 0; i < ruhak.length; i++) {
+            if (ruhak[i] != null) {
+                System.out.println("Ruha: " + ruhak[i]);
+            }
+        }
+    }
 }
-
-
-
